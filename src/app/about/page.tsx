8a898fe4dfc79b602dbf16d70847ac97cd9dc180
espinozagabriel0 <bascopeespinozagabriel@fpllefia.com>
2025-06-03 18:11:51 +0200

    componente header , vista about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+  return (
+    <div>
+      Crea una web que, mitjançant l’API ‘The Movie Database (TMDB)’ et permeti
+      mostrar les pel·lícules populars de la seva base de dades.
+    </div>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,33 @@
+"use client";
+import Link from "next/link";
 
 export default function Header() {
+
   return (
-    <header className="p-5">
-      gabriel
+    <header className="flex h-16 w-full items-center justify-between bg-background">
+      <Link href="/" className="flex items-center gap-2" prefetch={false}>
+        <span className="text-lg font-medium">Movie Explorer</span>
+      </Link>
+      <nav className="hidden items-center gap-6 lg:flex">
+        <Link
+          href="/"
+          className={
+            "text-sm font-medium transition-colors hover:text-primary text-slate-800"
+          }
+          prefetch={false}
+        >
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className={
+            "text-sm font-medium transition-colors hover:text-primary text-slate-800"
+          }
+          prefetch={false}
+        >
+          About
+        </Link>
+      </nav>
     </header>
-  )
+  );
 }
